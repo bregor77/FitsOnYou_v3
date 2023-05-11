4a4a09de5b4cfde5cff8a202233df87003ee1596
bregor77 <bregor77@gmail.com>
2023-05-11 21:04:05 +0100

    initial commit

---
+++ FitsOnYou/Admin/Dashboard.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FitsOnYou.Admin
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Session["breadCrum"] = "";
+            }
+        }
+    }
+}
